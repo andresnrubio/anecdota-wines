98bdcb448d534fb04a848f9f6695f98c72faa91d
Andres Rubio <rubioandres1@gmail.com>
2023-10-23 23:03:22 -0300

    Correccion nombres

---
+++ src/assets/index.js
@@ -0,0 +1,63 @@
+import blend_bg from './blend.webp';
+import cabernet_franc_bg from './cabernet_franc.webp';
+import malbec_bg from './malbec_bg.webp';
+import logotipo from './logotipo.png';
+import isotipo from './isotipo.png';
+import vainilla_icon from './vainilla.png';
+import dark_chocolate from './chocolate.png';
+import raspberry from './frambuezas.png';
+import black_berry from './frutos_negros.png';
+import red_berry from './frutos_rojos.png';
+import cumin from './comino.png';
+import liquorice from './regaliz.png';
+import green_bell_pepper from './pimiento_verde.png';
+import black_pepper from './pimienta.png';
+import color_icon from './Color.png';
+import thermometer_icon from './termometro.png';
+import eucalyptus_icon from './Laurel.png';
+import malbec_fruit_icon from './malbec_fruit.png';
+import cabernet_fruit_icon from './cabernet.png';
+import cabernet_franc_fruit_icon from './cabernet_franc_fruit.png';
+import bonarda_fruit_icon from './bonarda.png';
+import butter_icon from './manteca.png';
+import cherry_icon from './Cereza.png';
+import bottle_blend from './botella_blend.png';
+import bottle_cabernet from './botella_cabernet_franc.png';
+import bottle_malbec from './botella_malbec.png';
+import map_mendoza from './mapa_mendoza.png';
+import map_arg from './mapa_arg.png';
+import topografico from './topografico.png';
+import dot from './orangeDot.png';
+
+export {
+  logotipo,
+  isotipo,
+  blend_bg,
+  cabernet_franc_bg,
+  malbec_bg,
+  vainilla_icon,
+  color_icon,
+  cumin,
+  raspberry,
+  black_berry,
+  red_berry,
+  dark_chocolate,
+  liquorice,
+  black_pepper,
+  green_bell_pepper,
+  thermometer_icon,
+  cabernet_fruit_icon,
+  cabernet_franc_fruit_icon,
+  malbec_fruit_icon,
+  bonarda_fruit_icon,
+  eucalyptus_icon,
+  butter_icon,
+  cherry_icon,
+  bottle_blend,
+  bottle_cabernet,
+  bottle_malbec,
+  map_arg,
+  map_mendoza,
+  topografico,
+  dot,
+};
